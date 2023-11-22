60ddcf0a9a14e3e8e82ec00d17ba11e42dbeddb7
Th1nk <Tobiasransborg@hotmail.com>
2023-11-22 09:04:04 +0100

    za commit

---
+++ mainPage/sketch.js
@@ -0,0 +1,21 @@
+let currentPage = '#page1'
+
+function setup() {
+  select('#item1').mouseClicked( () => switchPage('#page1') )
+  select('#item2').mouseClicked( () => switchPage('#page2') )
+  select('#item3').mouseClicked( () => switchPage('#page3') )
+  select('#item4').mouseClicked( () => switchPage('#page4') )
+}
+
+function switchPage(whichPage){
+  select(currentPage).removeClass('show')
+  select(whichPage).addClass('show')
+  currentPage = whichPage
+}
+
+function draw() {
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
