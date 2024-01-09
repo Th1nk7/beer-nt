4acb753a747de63c54748cc159f71a0863e5a5ba
Th1nk <Tobiasransborg@hotmail.com>
2024-01-09 20:19:47 +0100

    aaadome

--- mainPage/sketch.js
+++ mainPage/sketch.js
@@ -1,4 +1,5 @@
 let currentPage = '#page1'
+let side4nummer = 1
 
 function setup() {
   select('#item1').mouseClicked( () => switchPage('#page1') )
@@ -15,7 +16,20 @@ function switchPage(whichPage){
   currentPage = whichPage
 }
 
-function draw() {
+let toggle = () => {
+
+  let side1 = "#page4side1"
+  let side2 = "#page4side2"
+
+  if(side4nummer===1){
+    select(side1).removeClass('beGone')
+    select(side2).addClass('beGone')
+    side4nummer = 2
+  } else if(side4nummer===2){
+    select(side1).addClass('beGone')
+    select(side2).removeClass('beGone')
+    side4nummer = 1
+  }
 }
 
 function windowResized(){
